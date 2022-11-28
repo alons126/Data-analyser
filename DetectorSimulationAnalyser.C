788c84861e0761cf635c71cc2182005b42a4b624
alons126 <102910135+alons126@users.noreply.github.com>
2022-11-28 16:53:17 +0200

    ff

--- DetectorSimulationAnalyser.C
+++ DetectorSimulationAnalyser.C
@@ -5355,9 +5355,10 @@ void EventAnalyser(int NumberOfProtons, int NumberOfNeutrons) {
 
 //        c12.addExactPid(2212, NumberOfProtons); //exactly 2 protons
         c12.addExactPid(11, 1); //exactly 1 electron
+        c12.addExactPid(2212, 1); //exactly 1 electron
 
-        c12.addAtLeastPid(2212,1);
-        c12.addAtLeastPid(2112,0);
+//        c12.addAtLeastPid(2212,1);
+//        c12.addAtLeastPid(2112,0);
 
         c12.addZeroOfRestPid(); //nothing else
 
