5b9927aab5b1d44b2536f3878e3d6c1c8d6888cc
alons126 <102910135+alons126@users.noreply.github.com>
2023-01-29 09:54:32 +0200

    J test 1

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
-run(){
-#echo
-#echo "Moving to '/genie/app/users/asportes/GENIE/Generator/'..."
-#echo
-#cd /genie/app/users/asportes/GENIE/Generator/
-#echo
-#echo "Pulling brunch..."
-#echo
-#git pull https://github.com/alons126/Generator.git asportes_master_2p_SuSAv2
-#echo
-#echo "Pulling finished!"
-#echo
-}
+#!/bin/bash
+
+INPUT=""
+
+clas12root -b -q ./main.c
+
+#runAnalyser(){
+#clas12root -b -q ./main.c
+#}
