951ef8e96cbaaf08bf9598ae2dcb123c9dabc0c6
alons126 <102910135+alons126@users.noreply.github.com>
2023-02-01 15:07:48 +0200

    j last test 2

--- codeSetup.h
+++ codeSetup.h
@@ -49,18 +49,16 @@ std::string file_name = "general_file";
 //std::string AnalyseFileSample = "recon_qe_GENIE_C_598636MeV_Q2_0_4_test_2_first_10"; // my test with Justin's code
 ////std::string AnalyseFileSample = "recon_qe_GENIE_C_598636MeV_Q2_0_4_test_2_first_100"; // my test with Justin's code
 ////std::string AnalyseFileSample = "recon_qe_GENIE_C_598636MeV_Q2_0_4_test_2"; // my test with Justin's code
-//
+
 std::string AnalyseFilePath = "volatile/clas12/users/esteejus/Simulation_sigmaCM";
 std::string AnalyseFileSample = "reconhipo"; // Justin's MC files
-//
+
 ////std::string AnalyseFileSample = "recon_qe_GENIE_C_598636MeV_Q2_0_4_test_1"; // my test with Josh's code
 ////std::string AnalyseFileSample = "recon_c12_6gev";
-//std::string AnalyseFileDir = "/" + AnalyseFilePath + "/" + AnalyseFileSample + "/";
+std::string AnalyseFileDir = "/" + AnalyseFilePath + "/" + AnalyseFileSample + "/";
 
-//    string AnalyseFileDirContent = AnalyseFileDir + "recon_qe_gcf_c_5.98gev_sigmacm_200_1*.hipo";
 string AnalyseFileDirContent = AnalyseFileDir + "recon_qe_gcf_c_5.98gev_sigmacm_200_1*.hipo";
-
-std::string AnalyseFile = AnalyseFileDir + "*.hipo";
+//string AnalyseFileDirContent = AnalyseFileDir + "*.hipo";
 
 
 ////// root files:
