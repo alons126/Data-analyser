fdf1b2464e5b155e761515385a2d7dd72d1e331d
alons126 <102910135+alons126@users.noreply.github.com>
2023-03-28 20:15:01 +0300

    1

--- TruthLevelAnalyser.c
+++ TruthLevelAnalyser.c
@@ -7071,7 +7071,7 @@ void gst::Loop() {
 //        gStyle->SetStatX(0.88);
 //        gStyle->SetStatY(0.4);
         theta_lp_VS_phi_lp->SetStats(0);
-        c1->SaveAs("" + WorkingDirectory + "plots/Theta_l_VS_Phi_l_inclusive.png");
+        c1->SaveAs((WorkingDirectory + "plots/Theta_l_VS_Phi_l_inclusive.png").c_str());
 //        gStyle->SetStatX(DefStatX);
 //        gStyle->SetStatY(DefStatY);
         c1->Clear();
