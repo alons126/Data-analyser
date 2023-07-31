8a273826dc6b97c04930ed512bd0e6dfa771ebb9
alons126 <102910135+alons126@users.noreply.github.com>
2023-07-31 19:07:20 +0300

    Ar40 run stage 00 T2

--- DetectorSimulationAnalyser.C
+++ DetectorSimulationAnalyser.C
@@ -130,13 +130,13 @@ void EventAnalyser() {
     bool ES_by_leading_FDneutron = true;
 
     /* Acceptance maps settings */
-    bool generate_AMaps = true; // Generate acceptance maps
+    bool generate_AMaps = false; // Generate acceptance maps
     bool TL_with_one_reco_electron = true;
     bool reformat_e_bins = false;
     bool equi_P_e_bins = true;
 
     /* Neutron resolution settings */
-    bool plot_and_fit_MomRes = false;
+    bool plot_and_fit_MomRes = true;
     bool VaryingDelta = false;
     double DeltaSlices = 0.05;
 
