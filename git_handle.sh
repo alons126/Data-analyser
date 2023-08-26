2e87734cdf263b3e8d058dc740f37f071a0314db
alons126 <102910135+alons126@users.noreply.github.com>
2023-08-26 09:04:54 +0300

    C12x4_G18_02a_00_000_Q204_598636MeV S02 T2

---
+++ git_handle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export ""
+git add *
+export ""
+export "- git files added -----------------------------------------------------"
+export ""
+git stash
+export ""
+export "- git files stashed ---------------------------------------------------"
+export ""
+git pull
+export ""
+export "- git pull completed --------------------------------------------------"
+export ""
