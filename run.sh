cd0f850681e4976b3105931e83b19fa2de78d270
alons126 <102910135+alons126@users.noreply.github.com>
2023-08-26 09:18:20 +0300

    C12x4_G18_02a_00_000_Q204_598636MeV S02 T1

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# INPUT=""
-
+echo ""
+echo "- Lunching git handling script ----------------------------------------"
+echo ""
+source git_handle.sh
+echo ""
+echo "- Re-pulling repository -----------------------------------------------"
+echo ""
 git reset --hard
 git pull
+echo ""
+echo "- Lunching data-analysis code -----------------------------------------"
+echo ""
 clas12root -b -q ./main.c
-
-#runAnalyser(){
-#clas12root -b -q ./main.c
-#}
+echo ""
+echo "- Operation finished --------------------------------------------------"
