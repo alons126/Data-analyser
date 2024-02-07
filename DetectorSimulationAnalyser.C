b36a91c20d14bf4b99f781010f5334db7e0141aa
alons126 <102910135+alons126@users.noreply.github.com>
2024-02-07 22:16:57 +0200

    end of day backup

--- DetectorSimulationAnalyser.C
+++ DetectorSimulationAnalyser.C
@@ -195,7 +195,7 @@ void EventAnalyser() {
     bool apply_nBeta_fit_cuts = true; // apply neutron upper mom. th.
     bool apply_fiducial_cuts = true;
     bool apply_kinematical_cuts = true;
-    bool apply_kinematical_weights = true;
+    bool apply_kinematical_weights = false;
     bool apply_nucleon_SmearAndCorr = true;
 
     //<editor-fold desc="Custom cuts naming & print out execution variables">
