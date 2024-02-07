e34a92dd3fea537aa14793f632f3688e439326e0
alons126 <102910135+alons126@users.noreply.github.com>
2024-02-07 17:21:35 +0200

    C12x4_data_6GeV S1 noNC T2

--- DetectorSimulationAnalyser.C
+++ DetectorSimulationAnalyser.C
@@ -196,7 +196,7 @@ void EventAnalyser() {
     bool apply_fiducial_cuts = false;
     bool apply_kinematical_cuts = false;
     bool apply_kinematical_weights = false;
-    bool apply_nucleon_SmearAndCorr = true;
+    bool apply_nucleon_SmearAndCorr = false;
 
     //<editor-fold desc="Custom cuts naming & print out execution variables">
 
